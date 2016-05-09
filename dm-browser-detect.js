a970bca30c7fdffa1bbfd3d5ed338851313502ca
derek.morash <derek.morash@gmail.com>
2016-05-09 11:31:39 -0300

    Updating JS, now supports ipad ipod and iphone. Safari not ready yet

--- dm-browser-detect.js
+++ dm-browser-detect.js
@@ -7,14 +7,31 @@ var dmDetectBrowser = (function() {
     //loop through each element
     [].forEach.call(hideEl, function(el) {
       // get the selected browsers in the data attribute
-      var selectedBrowsers = el.dataset.dmDetectBrowsers.split(" ");
+      var selectedBrowsers = el.dataset.dmDetectBrowsers
+        .toLowerCase().split(" ");
 
       //loop through each slected browser
       selectedBrowsers.forEach(function(thisBrowser) {
+
         // if the browser is one of the elements attributes
-        if(userBrowser.indexOf(thisBrowser) > -1) {
+        if(userBrowser.indexOf(thisBrowser) > -1 && thisBrowser != 'safari' && thisBrowser != 'ios') {
+            el.className += ' dm-detect-hidden';
+        } else if(thisBrowser === 'safari' &&
+        userBrowser.indexOf(thisBrowser) &&
+        userBrowser.indexOf('ipad') < 0 &&
+        userBrowser.indexOf('ipod') < 0 &&
+        userBrowser.indexOf('iphone') < 0) {
+          //if the slected browser is safari, but the current browser isnt a mobile ios device
+          el.className += ' dm-detect-hidden';
+        } else if(thisBrowser === 'ios' &&
+        userBrowser.indexOf('ipad') > -1 &&
+        userBrowser.indexOf('ipod') > -1 &&
+        userBrowser.indexOf('iphone') > -1 &&
+        userBrowser.indexOf(!window.MSStream) > -1) {
+          //if the selected browser is ios
           el.className += ' dm-detect-hidden';
         }
+
       }); //foreach selected browser
     }); //foreach el
   }; //hideAll
@@ -25,12 +42,27 @@ var dmDetectBrowser = (function() {
       //set the element to hidden by default
       el.className += ' dm-detect-hidden';
       // get the selected browsers in the data attribute
-      var selectedBrowsers = el.dataset.dmDetectBrowsers.split(" ");
+      var selectedBrowsers = el.dataset.dmDetectBrowsers
+        .toLowerCase().split(" ");
 
       //loop through each slected browser
       selectedBrowsers.forEach(function(thisBrowser) {
-        // if the browser is one of the selected browsers
-        if(userBrowser.indexOf(thisBrowser) > -1) {
+        // if the browser is one of the elements attributes
+        if(userBrowser.indexOf(thisBrowser) > -1 && thisBrowser != 'safari' && thisBrowser != 'ios') {
+            el.className = el.className.replace('dm-detect-hidden', '');
+        } else if(thisBrowser === 'safari' &&
+        userBrowser.indexOf(thisBrowser) &&
+        userBrowser.indexOf('ipad') < 0 &&
+        userBrowser.indexOf('ipod') < 0 &&
+        userBrowser.indexOf('iphone')< 0 ) {
+          //if the slected browser is safari, but the current browser isnt a mobile ios device
+          el.className = el.className.replace('dm-detect-hidden', '');
+        } else if(thisBrowser === 'ios' &&
+        userBrowser.indexOf('ipad') > -1 &&
+        userBrowser.indexOf('ipod') > -1 &&
+        userBrowser.indexOf('iphone') > -1 &&
+        userBrowser.indexOf(!window.MSStream) > -1) {
+          //if the selected browser is ios
           el.className = el.className.replace('dm-detect-hidden', '');
         }
       }); //foreach selected browser
