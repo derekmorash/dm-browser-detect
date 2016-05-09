f2d5081b88096c53c9e8fbd11ae7f7bf9b7fe844
derek.morash <derek.morash@gmail.com>
2016-05-09 11:33:20 -0300

    I jumped the gun on the version number. It's really more like 0.1.2, soon t be v1

--- dm-browser-detect.php
+++ dm-browser-detect.php
@@ -3,7 +3,7 @@
 Plugin Name: DM Browser Detect
 Plugin URI:  https://github.com/derekmorash/dm-browser-detect
 Description: Display or hide content depending on selected browsers, using shortcodes
-Version:     1.0
+Version:     0.1.2
 Author:      Derek Morash
 Author URI:  https://github.com/derekmorash
 License:     GPL3
